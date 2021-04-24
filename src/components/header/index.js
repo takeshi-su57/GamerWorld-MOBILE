a737fb5be5a0c47ba251d30ad73332ce81079b66
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2021-04-23 22:39:42 -0300

    Tratando navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Image, TouchableOpacity } from 'react-native'
-import styles from './styles'
+import { View, Image, TouchableOpacity, StyleSheet} from 'react-native'
 
 import qrcode from '../../assets/qrcode.png'
 import logo from '../../assets/logoPurple.png'
 import sunny from '../../assets/sunny.png'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 
 function Header({back, img}){
     return(
@@ -28,5 +28,27 @@ function Header({back, img}){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    Container:{
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        padding: 20,
+        marginTop: getStatusBarHeight(),
+        width: '100%',
+        borderBottomWidth: 5,
+        borderBottomColor: '#B40486'
+    },
+    Logo:{
+        width: 80,
+        height: 80
+    },
+    Icon:{
+        width: 35,
+        height: 35
+    }
+})
+
 
 export default Header
