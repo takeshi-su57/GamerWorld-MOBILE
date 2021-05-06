914924226d40dd0792a72b3e33d15a54ceef07a0
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2021-05-06 15:44:40 -0300

    Inserindo novos personagens(Avatares)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import joker from '../assets/joker.png'
 import arrow from '../assets/arrow.png'
 import captainAmerica from '../assets/captain-america.png'
 import crash from '../assets/crash.png'
@@ -9,20 +10,77 @@ import mario from '../assets/mario.png'
 import naruto from '../assets/naruto.png'
 import magneto from '../assets/magneto.png'
 import sonic from '../assets/sonic.png'
+import batman from '../assets/batman.png'
+import bender from '../assets/bender.png'
+import deadpool from '../assets/deadpool.png'
+import cyclops from '../assets/cyclops-marvel.png'
+import gambit from '../assets/gambit.png'
+import freddy from '../assets/freddy-krueger.png'
+import hermione from '../assets/hermione.png'
+import hinata from '../assets/hinata.png'
+import hulk from '../assets/hulk.png'
+import jake from '../assets/jake.png'
+import jeangrey from '../assets/jean-grey.png'
+import lexluthor from '../assets/lex-luthor.png'
+import logan from '../assets/logan-x-men.png'
+import lukecage from '../assets/luke-cage.png'
+import minecraft from '../assets/minecraft.png'
+import mystique from '../assets/mystique.png'
+import nightwing from '../assets/nightwing.png'
+import professorfarnsworth from '../assets/professor-farnsworth.png'
+import purpleman from '../assets/purple-man.png'
+import rogue from '../assets/mystique.png'
+import spiderman from '../assets/spider-man.png'
+import stormmarvel from '../assets/storm-marvel.png'
+import thanos from '../assets/thanos.png'
+import transformer from '../assets/transformer.png'
+import walterwhite from '../assets/walter-white.png'
+import ugandanknuckles from '../assets/ugandan-knuckles.png'
+import zoidberg from '../assets/zoidberg.png'
+import wolverine from '../assets/wolverine.png'
 
 const images = [
     null,
-    arrow,
+    deadpool,
     captainAmerica,
+    arrow,
+    joker,
+    wolverine,
+    batman,  
+    spiderman,
+    mario,
     crash,
-    frodo,
     harryPotter,
     harleyquinn,
     jeanGrey,
-    mario,
+    logan,
     naruto,
     magneto,
-    sonic
+    sonic,
+    bender,
+    frodo,
+    cyclops,
+    gambit,
+    freddy,
+    hermione,
+    hinata,
+    hulk,
+    jake,
+    jeangrey,
+    lexluthor,
+    lukecage,
+    minecraft,
+    mystique,
+    nightwing,
+    professorfarnsworth,
+    purpleman,
+    rogue,
+    stormmarvel,
+    thanos,
+    transformer,
+    walterwhite,
+    ugandanknuckles,
+    zoidberg
 ]
 
 export default images
